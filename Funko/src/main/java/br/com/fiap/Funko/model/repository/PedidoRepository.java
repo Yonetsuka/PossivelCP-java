0714f7acee7234db52962350c640bbf800203340
Yonetsuka <127115211+Yonetsuka@users.noreply.github.com>
2024-10-22 11:35:41 -0300

    Delete Funko directory

--- Funko/src/main/java/br/com/fiap/Funko/model/repository/PedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.Funko.model.repository;
-
-import br.com.fiap.Funko.model.entity.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long>{
-}
